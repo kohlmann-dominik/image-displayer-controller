cc59147d4ca1ad26b29dd72b2c7c6c92ebe64138
itd-kohlmann <dominik.kohlmann@itdesign.at>
2025-12-03 12:24:49 +0100

    updated config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,11 +1,11 @@
 // src/config.ts
 
-// Basis-URL deines Backends im DEV
-const HOST = window.location.hostname // nimmt 127.0.0.1, 192.168.x.y, etc.
+export const API_BASE =
+  import.meta.env.PROD
+    ? ""   // Produktion → gleiche Origin: http://displaycontroller
+    : "http://localhost:4000" // Entwicklung → Backend direkt
 
-export const API_BASE = `http://${HOST}:4000`
-export const WS_URL = `ws://${HOST}:4000`
-
-// Später für dein Heimnetz z.B.:
-// export const API_BASE = "http://192.168.0.50:4000"
-// export const WS_URL = "ws://192.168.0.50:4000"
+export const WS_URL =
+  import.meta.env.PROD
+    ? `ws://${window.location.host}/ws` // Produktion → WebSocket via Nginx
+    : "ws://localhost:4000/ws" // Entwicklung
