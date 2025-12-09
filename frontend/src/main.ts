2fad1b450c562d12d154d0b3bb35a71b8c3cba84
Dominik Kohlmann <kohlmann.dominik@gmail.com>
2025-12-09 18:20:42 +0100

    test ios ausgetrickst

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,7 @@ import "./style.css"
 import App from "./App.vue"
 import { router } from "./router"
 
-const APP_VERSION = "0.3.7" // neue Version setzen
+const APP_VERSION = "0.3.8" // neue Version setzen
 
 const url = new URL(window.location.href)
 
